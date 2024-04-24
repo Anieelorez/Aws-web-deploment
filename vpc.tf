08539edad6b5ea9540bc6d1ed93a8b95789269a1
Anieelorez <anieelorez1@gmail.com>
2024-04-24 10:56:02 +0100

    modified vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,22 @@
 # create vpc
 # terraform aws create vpc
 resource "aws_vpc" "vpc" {
-  cidr_block              = "10.0.0.0/16"
-  instance_tenancy        = "default"
-  enable_dns_hostnames    = true
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
 
-  tags      = {
-    Name    = "custom-vpc"
+  tags = {
+    Name = "custom-vpc"
   }
 }
 
 # create internet gateway and attach it to vpc
 # terraform aws create internet gateway
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id    = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-  tags      = {
-    Name    = "int-gw"
+  tags = {
+    Name = "int-gw"
   }
 }
 
@@ -28,28 +28,28 @@ resource "aws_subnet" "public_subnet_az1" {
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
-  tags      = {
-    Name    = "public subnet Az1"
+  tags = {
+    Name = "public subnet Az1"
   }
 }
 
 
 resource "aws_route_table" "public_route_table" {
-  vpc_id       = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags       = {
-    Name     = "public route table"
+  tags = {
+    Name = "public route table"
   }
 }
 
 # associate public subnet az1 to "public route table"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id           = aws_subnet.public_subnet_az1.id
-  route_table_id      = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
